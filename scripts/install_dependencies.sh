9e7f627d901d924a1b5a82f97f096b5667dbbe3e
Rohit0509 <rohit@squareops.com>
2020-06-24 16:30:36 +0530

    DD

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-if which node > /dev/null
-then
-    echo "node is installed, skipping..."
-else
-    echo "Installing node ...."
-    sudo apt update
-    sudo apt-get install software-properties-common -y
-    curl -sL https://deb.nodesource.com/setup_14.x | bash -
-    sudo apt-get install nodejs -y
-    sudo npm i -g pm2 
-fi
+# if which node > /dev/null
+# then
+#     echo "node is installed, skipping..."
+# else
+#     echo "Installing node ...."
+#     sudo apt update
+#     sudo apt-get install software-properties-common -y
+#     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+#     sudo apt install nodejs -y
+#     sudo npm i -g pm2 
+# fi
+
 cd /apps
 npm install
